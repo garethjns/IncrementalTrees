5e980149bf5aabc0427ead6cb760f96bd0eebbc8
Gareth Jones <garethjns4@gmail.com>
2019-02-24 17:51:44 +0000

    v0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 #     long_description = fh.read()
 
 setuptools.setup(name='incremental_trees',
-                 version="0.1",
+                 version="0.3",
                  author="Gareth Jones",
                  author_email="author@example.com",
                  description='Sklearn forests with partial fits',
