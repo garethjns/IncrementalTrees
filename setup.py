a6958e12da61c773ce437329d5387a8eb546f436
Gareth Jones <garethjns4@gmail.com>
2020-05-23 11:26:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,11 @@ import setuptools
 
 from incremental_trees import __version__
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(name='incremental_trees',
                  version=__version__,
                  author="Gareth Jones",
-                 author_email="author@example.com",
+                 author_email="garethgithub@gmail.com",
                  description='Sklearn forests with partial fits',
-                 long_description=long_description,
                  long_description_content_type="text/markdown",
                  packages=setuptools.find_packages(),
                  url="https://github.com/garethjns/IncrementalTrees",
